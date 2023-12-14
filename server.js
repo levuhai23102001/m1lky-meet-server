4ab10b284635ae03dc24de4ea99702373038f799
vuhai2310 <lvhai.19it4@vku.udn.vn>
2023-12-15 06:03:57 +0700

    update url

--- server.js
+++ server.js
@@ -14,12 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.get("/test", (req, res) => {
   res.send("Hello World");
